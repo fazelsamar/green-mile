add2c0d87dc2cc7c2784373bc4153da5bf7bf040
Fazel <fazelsamar@gmail.com>
2022-03-29 14:17:30 +0430

    Some changes

--- location/views.py
+++ location/views.py
@@ -1,8 +1,4 @@
-from rest_framework import permissions, status, generics
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
+from rest_framework import permissions, generics
 
 from . import serializers
 from . import models
